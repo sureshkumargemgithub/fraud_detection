4c8e23b9d0c8969bb7431dcf1aea0ba6c84d8d7e
sureshkumargemgithub <sureshkumargem3@gmail.com>
2021-12-21 00:00:44 +0530

    actual change now to see whether there is conflict

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
 # I am trying to write one more python code file
 print ("Not participating in test")
-print ("this is just to see whether there is conflict coming or not")
+print ("this is just to see whether there is conflict or not")
 print ("This line also not participating in test")
